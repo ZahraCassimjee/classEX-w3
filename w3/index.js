e6a468c672986cbf8ec6b574e20607058798c257
ZahraCassimjee <231131@virtualwindow.co.za>
2023-08-01 11:21:27 +0200

    .

--- w3/index.js
+++ w3/index.js
@@ -2,8 +2,22 @@ $(document).ready(function () {
 
     $("#test").click(function(){
 
-        $("h1").hide();
+        // $("h1").hide();
+        // s
+        //     {
+        //         color: "red"
+        //     }
+        //    )
+
+    // console.log("Hello Im doing it");
+    //     alert("Hello World!");
+        // $(".test2").append("<h2>The glory days are back</h2>")
+ 
+ $("h1").addClass("newClass")
+ 
+ 
     });
+
 
 
 });
